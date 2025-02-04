3b9b5b59992592cfbf42c89664dbd10aac33df4a
Minseo Park <hightuv@gmail.com>
2025-02-04 17:01:15 +0900

    Modify router.js
    - handler에 존재하지 않는 함수면 404로 처리

--- 3주차/02-04/WebStart/router.js
+++ 3주차/02-04/WebStart/router.js
@@ -1,6 +1,18 @@
-function route(pathname, handle) {
+function route(pathname, handle, response) {
+  /* if (pathname === '/favicon.ico') {
+    response.writeHead(204);
+    response.end();
+    return;
+  }
+   */
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname](response);
+  } else {
+    response.writeHead(404);
+    response.write('Not Found');
+    response.end();
+  }
   console.log('pathname : ' + pathname);
-  handle[pathname]();
 }
 
 exports.route = route;
