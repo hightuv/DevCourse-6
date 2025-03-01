464257517c8d8de9ae8dd75ac14738d2a1dba986
Minseo Park <hightuv@gmail.com>
2025-03-01 18:39:14 +0900

    02-27 실습내용

---
+++ 2월/6주차/02-27/youtube-demo/mariadb.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'yourpassword',
+  database: 'Youtube',
+  dateStrings: true
+});
+
+db.connect(err => {
+  if (err) {
+    console.log(err)
+    console.log('DB 연결 실패');
+  } else {
+    console.log('DB 연결 성공');
+  }
+});
+
+module.exports = db;
